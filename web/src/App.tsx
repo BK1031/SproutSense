dfd0d88270c09da12217c0b980e95f8a3a03a6d3
Bharat Kathi <bharat1031@gmail.com>
2025-01-10 12:44:16 -0800

    initialize web

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen flex-col justify-between">
+        <div className="flex h-screen flex-col justify-start p-4 lg:p-32 lg:pt-16">
+          <h2>SproutSense</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
