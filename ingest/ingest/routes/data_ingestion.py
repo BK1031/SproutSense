e4df4a7e673546bbbbc0dee92f9e73ad73188ebd
Pablo <pds@ucsb.edu>
2025-02-17 06:36:06 -0800

    refactored ingestion route

--- ingest/ingest/routes/data_ingestion.py
+++ ingest/ingest/routes/data_ingestion.py
@@ -3,155 +3,71 @@ from  .. import db, app
 from ..models.sensor_data import SensorData
 import struct
 
-BYTE_ARRAY_FORMAT = ">HHHHBBlH"
+BYTE_ARRAY_CASES = {
+    'case_1':{
+        'format': ">HBBHH",
+        'sensor_data': ['module_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1'],
+        'size': struct.calcsize(">HBBHH")
+    },
+    'case_2':{
+        'format': ">HBBHHHl",
+        'sensor_data': ['module_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
+                         'nitrogen', 'soil_moisture'],
+        'size': struct.calcsize(">HBBHHHl")
+    },
+    'case_3':{
+        'format': ">HBBHHHlHH",
+        'sensor_data': ['module_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
+                         'nitrogen', 'soil_moisture', 'phosphorus', 'potassium'],
+        'size': struct.calcsize(">HBBHHHlHH")
+    },
+    'case_4':{
+        'format': ">HBBHHHlHHlBlB",
+        'sensor_data': ['module_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
+                         'nitrogen', 'soil_moisture', 'phosphorus', 'potassium', 'latitude',
+                         'north_or_south', 'longitude', 'east_or_west'],
+        'size': struct.calcsize(">HBBHHHlHHlBlB")
+    },
+    
+}
 
-BYTE_ARRAY_FORMAT_1 = ">HBBHH"
+def process_payload(sensor_data):
 
-BYTE_ARRAY_FORMAT_2 = ">HBBHHHl"
+    payload_size = len(sensor_data)
 
-BYTE_ARRAY_FORMAT_3 = ">HBBHHHlHH"
+    payload_case_name = next(
+        (name for name, case in BYTE_ARRAY_CASES.items() if case['size'] == payload_size),
+        None
+    )
 
-BYTE_ARRAY_FORMAT_4 = ">HBBHHHlHHlBlB"
+    if not payload_case_name:
+        return {"eror": "Not a valid payload size"}, 400
+    
+    try:
+        payload_case = BYTE_ARRAY_CASES[payload_case_name]
+        upacked_data = struct.unpack(payload_case['format'], sensor_data)
 
+        # create an object for the data ingested and add to db
+        # db.session.add(ingested_data)
+        # db.session.commit()
 
+        processed_data = dict(zip(payload_case['sensor_data'], upacked_data))
 
-BYTE_ARRAY_SIZE = struct.calcsize(BYTE_ARRAY_FORMAT)
+        return {
+            "message": "Sensor Data Processed Successfully",
+            "data": processed_data
+        }, 200
 
-BYTE_ARRAY_SIZE_CASE_1 = struct.calcsize(BYTE_ARRAY_FORMAT_1)
+    except struct.error as e:
+        return {"error": f"Failed to unpack data: {str(e)}"}, 400
 
-BYTE_ARRAY_SIZE_CASE_2 = struct.calcsize(BYTE_ARRAY_FORMAT_2)
-
-BYTE_ARRAY_SIZE_CASE_3 = struct.calcsize(BYTE_ARRAY_FORMAT_3)
-
-BYTE_ARRAY_SIZE_CASE_4 = struct.calcsize(BYTE_ARRAY_FORMAT_4)
 
 ingest_bp = Blueprint('data_ingestion', __name__)
 
-@ingest_bp.route('/ingestCases', methods = ['POST'])
-def ingestCases():
-    base_station_data = request.data.strip()
-
-    if len(base_station_data) == BYTE_ARRAY_SIZE_CASE_1:
-        try:
-            unpacked_data = struct.unpack(BYTE_ARRAY_FORMAT_1, base_station_data)
-            id, Temp, Humidity, lux_ch0, lux_ch1 = unpacked_data
-
-            return jsonify({
-                "message": "Data ingested successfully",
-                "data": {
-                    "Sensor Module ID": id,
-                    "Temp": Temp,
-                    "Humidity": Humidity,
-                    "Lux_Ch0": lux_ch0,
-                    "Lux_Ch1": lux_ch1
-                }
-            }), 200
-
-        except struct.error as e:
-            return jsonify({"error": f"Failed to unpack data: {str(e)}"}), 400
-    
-    if len(base_station_data) == BYTE_ARRAY_SIZE_CASE_2:
-        try:
-            unpacked_data = struct.unpack(BYTE_ARRAY_FORMAT_2, base_station_data)
-            id, Temp, Humidity, lux_ch0, lux_ch1, nitrogen, soil_moisture = unpacked_data
-
-            return jsonify({
-                "message": "Data ingested successfully",
-                "data": {
-                    "Sensor Module ID": id,
-                    "Temp": Temp,
-                    "Humidity": Humidity,
-                    "Lux_Ch0": lux_ch0,
-                    "Lux_Ch1": lux_ch1,
-                    "nitrogen": nitrogen,
-                    "soil moisture": soil_moisture
-                }
-            }), 200
-
-        except struct.error as e:
-            return jsonify({"error": f"Failed to unpack data case 2: {str(e)}"}), 400
-    
-    if len(base_station_data) == BYTE_ARRAY_SIZE_CASE_3:
-        try:
-            unpacked_data = struct.unpack(BYTE_ARRAY_FORMAT_3, base_station_data)
-            id, Temp, Humidity, lux_ch0, lux_ch1, nitrogen, soil_moisture,phosphorus, potassium= unpacked_data
-
-            return jsonify({
-                "message": "Data ingested successfully",
-                "data": {
-                    "Sensor Module ID": id,
-                    "Temp": Temp,
-                    "Humidity": Humidity,
-                    "Lux_Ch0": lux_ch0,
-                    "Lux_Ch1": lux_ch1,
-                    "nitrogen": nitrogen,
-                    "soil moisture": soil_moisture,
-                    "phosphorus": phosphorus,
-                    "potassium" : potassium
-                }
-            }), 200
-
-        except struct.error as e:
-            return jsonify({"error": f"Failed to unpack data case 3: {str(e)}"}), 400
-    
-    if len(base_station_data) == BYTE_ARRAY_SIZE_CASE_4:
-        print("we are in case 4")
-        try:
-            unpacked_data = struct.unpack(BYTE_ARRAY_FORMAT_4, base_station_data)
-            id, Temp, Humidity, lux_ch0, lux_ch1, nitrogen, soil_moisture,phosphorus, potassium, latitude, north_or_south, longitude, east_or_west= unpacked_data
-
-            return jsonify({
-                "message": "Data ingested successfully",
-                "data": {
-                    "Sensor Module ID": id,
-                    "Temp": Temp,
-                    "Humidity": Humidity,
-                    "Lux_Ch0": lux_ch0,
-                    "Lux_Ch1": lux_ch1,
-                    "nitrogen": nitrogen,
-                    "soil moisture": soil_moisture,
-                    "phosphorus": phosphorus,
-                    "potassium" : potassium,
-                    "latidue": latitude,
-                    "north or south": north_or_south,
-                    "longitude": longitude,
-                    "east or west": east_or_west
-                }
-            }), 200
-
-        except struct.error as e:
-            return jsonify({"error": f"Failed to unpack data case 3: {str(e)}"}), 400
-    return jsonify({"error": "Invalid data size"}), 400
-
-
 @ingest_bp.route('/ingest', methods = ['POST'])
-def ingest():
-    base_station_data = request.data.strip()
+def ingestCases():
+    sensor_data = request.data.strip()
+    response, status_code = process_payload(sensor_data)
 
-    if len(base_station_data) != BYTE_ARRAY_SIZE:
-        return jsonify({"error": "Invalid data size"}), 400
-    
-    try:
-        unpacked_data = struct.unpack(BYTE_ARRAY_FORMAT, base_station_data)
-        id, N, P, K, Temp, Humidity, SoilMoisture, UV = unpacked_data
-        print(f"this is at test for id {id}")
-        new_sensor_data = SensorData(nitrogen=N, phosphorus=P, potassium=K,
-                                    humidity=Humidity, soilmoisture=SoilMoisture, temperature=Temp, uv=UV,sensormoduleid = id)
-        db.session.add(new_sensor_data)
-        db.session.commit()
+    return jsonify(response), status_code
 
-        return jsonify({
-            "message": "Data ingested successfully",
-            "data": {
-                "N": N,
-                "P": P,
-                "K": K,
-                "Temp": Temp,
-                "Humidity": Humidity,
-                "SoilMoisture": SoilMoisture,
-                "UV": UV
-            }
-        }), 200
-
-    except struct.error as e:
-        return jsonify({"error": f"Failed to unpack data: {str(e)}"}), 400
