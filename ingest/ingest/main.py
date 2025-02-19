e48623cca38b36bbe1572a208e4f7ece4329ebd1
Bharat Kathi <bharat1031@gmail.com>
2025-02-19 07:30:20 -0800

    ingest: fix main

--- ingest/ingest/main.py
+++ ingest/ingest/main.py
@@ -4,10 +4,10 @@ from ingest.models.base_station import BaseStation
 from ingest.models.sensor_readings import SensorReadings
 from ingest.models.sensor_module import SensorModule
 
-
-
-if __name__ == "__main__":
+def main():
     with app.app_context():
         db.create_all()
     app.run(debug=True)
 
+if __name__ == "__main__":
+    main()
