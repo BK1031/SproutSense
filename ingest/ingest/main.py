3f7a449a5ea8549768fab537473aa3656e424187
Pablo <pds@ucsb.edu>
2025-02-18 18:21:32 -0800

    adjusted format of byte array received and created tables for db

--- ingest/ingest/main.py
+++ ingest/ingest/main.py
@@ -1,9 +1,13 @@
 from . import app, db
 from ingest.models.sensor_data import SensorData
+from ingest.models.base_station import BaseStation
+from ingest.models.sensor_readings import SensorReadings
+from ingest.models.sensor_module import SensorModule
+
+
 
 if __name__ == "__main__":
     with app.app_context():
-        # db.drop_all()
         db.create_all()
     app.run(debug=True)
 
--- ingest/ingest/routes/data_ingestion.py
+++ ingest/ingest/routes/data_ingestion.py
@@ -1,32 +1,35 @@
 from flask import Blueprint, jsonify, request
 from  .. import db, app
 from ..models.sensor_data import SensorData
+from ..models.base_station import BaseStation
+from ..models.sensor_module import SensorModule
 import struct
 
 BYTE_ARRAY_CASES = {
     'case_1':{
-        'format': ">HBBHH",
-        'sensor_data': ['module_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1'],
-        'size': struct.calcsize(">HBBHH")
+        # i need to change the format since we are now also transmitting the case_id with it
+        'format': ">HHBBBHH",
+        'sensor_data': ['base_station_id', 'module_id', 'case_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1'],
+        'size': struct.calcsize(">HHBBBHH")
     },
     'case_2':{
-        'format': ">HBBHHHl",
-        'sensor_data': ['module_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
+        'format': ">HHBBBHHHl",
+        'sensor_data': ['base_station_id', 'module_id', 'case_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
                          'nitrogen', 'soil_moisture'],
-        'size': struct.calcsize(">HBBHHHl")
+        'size': struct.calcsize(">HHBBBHHHl")
     },
     'case_3':{
-        'format': ">HBBHHHlHH",
-        'sensor_data': ['module_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
+        'format': ">HHBBBHHHlHH",
+        'sensor_data': ['base_station_id', 'module_id', 'case_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
                          'nitrogen', 'soil_moisture', 'phosphorus', 'potassium'],
-        'size': struct.calcsize(">HBBHHHlHH")
+        'size': struct.calcsize(">HHBBBHHHlHH")
     },
     'case_4':{
-        'format': ">HBBHHHlHHlBlB",
-        'sensor_data': ['module_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
+        'format': ">HHBBBHHHlHHlBlB",
+        'sensor_data': ['base_station_id', 'module_id', 'case_id', 'temperature', 'humidity', 'lux_ch0', 'lux_ch1',
                          'nitrogen', 'soil_moisture', 'phosphorus', 'potassium', 'latitude',
                          'north_or_south', 'longitude', 'east_or_west'],
-        'size': struct.calcsize(">HBBHHHlHHlBlB")
+        'size': struct.calcsize(">HHBBBHHHlHHlBlB")
     },
     
 }
@@ -47,16 +50,19 @@ def process_payload(sensor_data):
         payload_case = BYTE_ARRAY_CASES[payload_case_name]
         upacked_data = struct.unpack(payload_case['format'], sensor_data)
 
+
+
         # create an object for the data ingested and add to db
         # db.session.add(ingested_data)
         # db.session.commit()
 
         processed_data = dict(zip(payload_case['sensor_data'], upacked_data))
 
-        return {
-            "message": "Sensor Data Processed Successfully",
-            "data": processed_data
-        }, 200
+        # return {
+        #     "data": processed_data
+        # }, 200
+
+        return processed_data, 200
 
     except struct.error as e:
         return {"error": f"Failed to unpack data: {str(e)}"}, 400
@@ -68,6 +74,37 @@ ingest_bp = Blueprint('data_ingestion', __name__)
 def ingestCases():
     sensor_data = request.data.strip()
     response, status_code = process_payload(sensor_data)
+
+    base_station_id = response["base_station_id"]
+    sensor_module_id = response["module_id"]
+
+    base_station = BaseStation.query.get(base_station_id)
+    sensor_module = SensorModule.query.get(sensor_module_id)    
+
+    if base_station and sensor_module:
+        # add the columns for all the fields
+        x = 1
+    elif base_station and not sensor_module:
+        # create sensor module and add to db
+        sensor_module_name = "module 1"
+        new_sensor_module = SensorModule(id=sensor_module_id, name=sensor_module_name, latitude = 1000, longitude = 1000)
+        db.session.add(new_sensor_module)
+        db.session.commit()
+    elif not base_station and sensor_module:
+        # add base station to table
+        x = 1
+    else:
+        print("we enter this case because we have nothing on the db")
+        sensor_module_name = "module 1"
+        base_station_name = "base station 1"
+        new_sensor_module = SensorModule(id=sensor_module_id, name=sensor_module_name, latitude = 1000, longitude = 1000)
+        new_base_station = BaseStation(id=base_station_id, name=base_station_name, latitude = 1000, longitude = 1000)
+        db.session.add(new_sensor_module)
+        db.session.add(new_base_station)
+        db.session.commit()
+
+
+    print(f"b id: {base_station_id}, s_id: {sensor_module_id}")
 
     return jsonify(response), status_code
 
