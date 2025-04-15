74c71f3f57367735b83bbf3b5b95991cc444d7e9
Bharat Kathi <bharat1031@gmail.com>
2025-04-15 16:40:31 -0700

    web: added store

--- web/src/lib/store.ts
+++ web/src/lib/store.ts
@@ -1,1 +1,15 @@
 import createStore from "react-superstore";
+import { BaseStation, initBaseStation } from "@/models/base-station";
+import { initSensorModule, SensorModule } from "@/models/sensor-module";
+
+export const [
+  useSelectedBaseStation,
+  setSelectedBaseStation,
+  getSelectedBaseStation,
+] = createStore<BaseStation>(initBaseStation());
+
+export const [
+  useSelectedSensorModule,
+  setSelectedSensorModule,
+  getSelectedSensorModule,
+] = createStore<SensorModule>(initSensorModule());
