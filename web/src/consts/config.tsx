da418942fbb78750861f57ecdfb966e7b3a9bbef
Bharat Kathi <bharat1031@gmail.com>
2025-04-17 07:51:51 -0700

    web: update default endpoint to http

--- web/src/consts/config.tsx
+++ web/src/consts/config.tsx
@@ -1,6 +1,6 @@
 export const BACKEND_URL =
   import.meta.env.VITE_BACKEND_URL ||
-  "https://hamilton-ec2.gauchoracing.com:5050";
+  "http://hamilton-ec2.gauchoracing.com:5050";
 export const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 export const MAPBOX_LIGHT_STYLE = "mapbox://styles/mapbox/standard";
 export const MAPBOX_DARK_STYLE = "mapbox://styles/mapbox/dark-v11";
